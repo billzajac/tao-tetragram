7d70791b940e252827ed38778f8db0cd5f406eaa
William Zajac <william.zajac@disney.com>
2016-04-09 20:08:09 -0700

    added passage number

--- gen-tetragram-server.go
+++ gen-tetragram-server.go
@@ -10,7 +10,19 @@ import (
 )
 
 func generateTetragram(w http.ResponseWriter, r *http.Request) {
+	base10Sum := 0
 	monograms := []string{"------", "--  --", "- - - "}
+	// 0000 -> 1
+	// 0001 -> 2
+	// 0002 -> 3
+	// 0010 -> 4
+	// 0011 -> 5
+	// 0012 -> 6
+	// 0020 -> 7
+	// 0021 -> 8
+	// 0022 -> 9
+	// 0100 -> 10
+	// 0101 -> 11
 
 	// TODO: figure out what the bigram chart number is from this also
 
@@ -22,14 +34,31 @@ func generateTetragram(w http.ResponseWriter, r *http.Request) {
 	// Tetra is four
 	for i := 0; i <= 3; i++ {
 		// choose random up from [0,3)
-		randInt, err := rand.Int(rand.Reader, big.NewInt(3))
+		randBigInt, err := rand.Int(rand.Reader, big.NewInt(3))
 		if err != nil {
 			panic(err)
 		}
 		// have to convert bigint back to int64 to use in index
-		randMonogram := monograms[randInt.Int64()]
+		randInt := int(randBigInt.Int64())
+
+		// To convert from trinary/ternary (base 3) to base 10, multiply each part by it's order of magnitude
+		switch i {
+		case 0:
+			base10Sum = base10Sum + randInt*27
+		case 1:
+			base10Sum = base10Sum + randInt*9
+		case 2:
+			base10Sum = base10Sum + randInt*3
+		case 3:
+			base10Sum = base10Sum + randInt*1
+		}
+
+		// have to convert bigint back to int64 to use in index
+		randMonogram := monograms[randBigInt.Int64()]
 		fmt.Fprintf(w, "%s\n", randMonogram)
 	}
+	// Remember to add 1 here because this system starts with 1, not 0
+	fmt.Fprintf(w, "\nPassage: %d\n", base10Sum+1)
 	fmt.Fprintf(w, "</pre></body></html>\n")
 }
 
--- gen-tetragram.rb
+++ gen-tetragram.rb
@@ -4,4 +4,23 @@ monograms = {0 => "------",
              1 => "--  --",
              2 => "- - - ",
              }
-(1..4).reduce([]) {|acc, i| acc << rand(3).to_i; acc}.map {|i| puts "#{monograms[i]}\n"}
+#(1..4).reduce([]) {|acc, i| acc << rand(3).to_i; acc}.map {|i| puts "#{monograms[i]}\n"}
+tetragram = (1..4).reduce([]) do |acc, i|
+  acc << rand(3).to_i
+  acc
+end
+
+# print out the lines
+tetragram.map {|i| puts "#{monograms[i]}\n"}
+
+# show the Passage number
+base10_sum = 0
+tetragram.each_with_index do |value, index|
+  case index
+     when 0 then base10_sum += value*27
+     when 1 then base10_sum += value*9
+     when 2 then base10_sum += value*3
+     when 3 then base10_sum += value*1
+  end
+end
+puts "\nPassage: #{base10_sum + 1}"
